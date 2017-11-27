07d34912101a6dcfdd5348a7d11034150c4a27a1
issam abbas <admin@mlk1.com>
2017-11-27 18:06:45 +0300

    add the methods

---
+++ index.js
@@ -0,0 +1,18 @@
+/*!
+ * console.log
+ * Copyright(c) 2017 mlk1.com
+ * Copyright(c) 2017 ISSAM ABBAS
+ * MIT Licensed
+ */
+
+(function(){
+    cl=function(data){
+      console.log(data);
+    };
+    ce=function(data){
+      console.error(data);
+    }
+  }());
+  
+  
+  
